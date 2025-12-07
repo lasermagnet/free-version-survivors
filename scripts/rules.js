55acad7405ba0d737eb91ecba71146f78bb0d59e
Bence Fodor <1785582+fodi@users.noreply.github.com>
2025-12-07 17:01:07 +0100

    b02-error

--- scripts/rules.js
+++ scripts/rules.js
@@ -1,27 +1,25 @@
 const rules = {
     // Player rules
+    "Player_Health_Start": 100,
     "Player": {
-        "Health": 100,
-        "NextLevelXP": 100
+        "Health": 100
     },
 
     // Spawn rules
     "Spawn": {
         "Item": {
-            "Cooldown": 10
-        },
-        "Enemy": {
-            "Cooldown": 2,
-            "Amount": 5
+            "Cooldown": 6
         }
     },
+
+    // Item rules
+    "Item_Spawn_Cooldown": 6,
 
     // Weapon rules
     "Weapons": {
         "Sword": {
             "Cooldown": 1,
-            "Level": 0,
-            "Damage": 10
+            "Level": 0
         }
     }
 }
