9e2e5d7d3f34e915a763748ae893fe412dcbdb24
Bence Fodor <1785582+fodi@users.noreply.github.com>
2025-12-07 17:07:47 +0100

    b62

--- scripts/_changelog.js
+++ scripts/_changelog.js
@@ -1,4 +1,7 @@
 /*
+Build 62
+- Drop objects are now always at the bottom of their layer
+
 Build 61
 - added Player.Bonus_Enemy_MovementSpeed_Multiplier
 - minor changes to "balance" and the start screen
