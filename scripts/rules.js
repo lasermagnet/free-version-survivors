9e96c009f3382148fc1dcc133f06011fa0f359f7
Bence Fodor <1785582+fodi@users.noreply.github.com>
2025-12-07 16:47:17 +0100

    b01

---
+++ scripts/rules.js
@@ -0,0 +1,15 @@
+const rules = {
+    // Player rules
+    "Player_Health_Start": 100,
+
+    // Item rules
+    "Item_Spawn_Cooldown": 3,
+
+    // Weapon rules
+    "Weapons": {
+        "Sword": {
+            "Cooldown": 1,
+            "Level": 0
+        }
+    }
+}
