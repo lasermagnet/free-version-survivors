9f652cc40b0e4376676c36f65dff615882a45b9e
Bence Fodor <1785582+fodi@users.noreply.github.com>
2025-12-07 17:06:14 +0100

    b46

--- scripts/_rules.js
+++ scripts/_rules.js
@@ -1,9 +1,10 @@
 import { Enemies } from "./R_Enemies.js"
 import { Level } from "./R_Level.js"
+import { Modifiers } from "./R_Modifiers.js"
 import { Player } from "./R_Player.js"
 import { Pools } from "./R_Pools.js"
 import { Upgrades } from "./R_Upgrades.js"
 import { Waves } from "./R_Waves.js"
 import { Weapons } from "./R_Weapons.js"
 
-export const rules = { Enemies, Level, Player, Pools, Upgrades, Waves, Weapons }
+export const rules = { Enemies, Level, Modifiers, Player, Pools, Upgrades, Waves, Weapons }
